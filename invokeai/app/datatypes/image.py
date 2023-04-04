8eb6ff9b685aa521d5479ce42396882bfad33141
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-04 11:05:15 +1000

    feat(nodes): add list_images endpoint
    - add `list_images` endpoint at `GET api/v1/images`
    - extend `ImageStorageBase` with `list()` method, implemented it for `DiskImageStorage`
    - add `ImageReponse` class to for image responses, which includes urls, metadata
    - add `ImageMetadata` class (basically a stub at the moment)
    - uploaded images now named `"{uuid}_{timestamp}.png"`
    - add `datatypes` module, moved a few classes into there. besides separating concerns more clearly, this helps to mitigate circular dependencies
    - improve thumbnail handling

---
+++ invokeai/app/datatypes/image.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel, Field
+
+from invokeai.app.datatypes.metadata import ImageMetadata
+
+
+class ImageType(str, Enum):
+    RESULT = "results"
+    INTERMEDIATE = "intermediates"
+    UPLOAD = "uploads"
+
+
+class ImageField(BaseModel):
+    """An image field used for passing image objects between invocations"""
+
+    image_type: str = Field(
+        default=ImageType.RESULT, description="The type of the image"
+    )
+    image_name: Optional[str] = Field(default=None, description="The name of the image")
+
+    class Config:
+        schema_extra = {
+            "required": [
+                "image_type",
+                "image_name",
+            ]
+        }
+
+
+class ImageResponse(BaseModel):
+    """The response type for images"""
+
+    image_type: ImageType = Field(description="The type of the image")
+    image_name: str = Field(description="The name of the image")
+    image_url: str = Field(description="The url of the image")
+    thumbnail_url: str = Field(description="The url of the image's thumbnail")
+    metadata: ImageMetadata = Field(description="The image's metadata")
